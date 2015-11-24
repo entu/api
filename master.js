0e638a98ac0fb94bbb277d71c40d31f477d77e76
Argo Roots <argo@roots.ee>
2015-11-24 14:30:52 +0200

    Log worker exit reason

--- master.js
+++ master.js
@@ -19,7 +19,13 @@ cluster.on('online', function(worker) {
 })
 
 // Listen for dying workers nad replace the dead worker, we're not sentimental
-cluster.on('exit', function(worker) {
-    console.log(new Date().toString() + ' worker ' + worker.id + ' died')
+cluster.on('exit', function(worker, code, signal) {
+    if(signal) {
+        console.error('Worker #' + worker.id + ' was killed by signal: ' + signal)
+    } else if(code !== 0) {
+        console.error('Worker #' + worker.id + ' exited with error code: ' + code)
+    } else {
+        console.error('Worker #' + worker.id + ' success!')
+    }
     cluster.fork()
 })
