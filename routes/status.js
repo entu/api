4b4c15dd8c53d0479803383f4acad0eef4eec78d
Argo Roots <argo@roots.ee>
2016-03-10 10:29:14 +0200

    Format status response for GeckoBoard

--- routes/status.js
+++ routes/status.js
@@ -1,4 +1,6 @@
+var _      = require('underscore')
 var async  = require('async')
+var op     = require('object-path')
 var router = require('express').Router()
 
 var entu   = require('../helpers/entu')
@@ -36,6 +38,47 @@ router.get('/requests', function(req, res) {
             		}
             	}
             ]).toArray(callback)
+        },
+        function(result, callback) {
+            var seriesData = {}
+
+            for (var variable in object) {
+                if(!object.hasOwnProperty(variable)) { continue }
+
+                var host = op.get(result, '_id.host')
+                var day = op.get(result, '_id.year') + '-' + ('00' + op.get(result, '_id.month')).substr(0, 2) + '-' + ('00' + op.get(result, '_id.day')).substr(0, 2)
+                var count = op.get(result, 'count')
+
+                op.set(seriesData, [host, 'name'], host)
+                op.push(seriesData, [host, 'data'], [day, count])
+                op.push(seriesData, [host, 'incomplete_from'], today.toISOString().substr(0, 7))
+            }
+            var graphData = {
+                x_axis: {
+                    type: 'datetime'
+                },
+                series: _.values(seriesData)
+                // [
+                //     {
+                //         name: 'Requests per day',
+                //         data: [
+                //             ['2014-01', 71173],
+                //             ['2014-02', 57624],
+                //             ['2014-03', 64851],
+                //             ['2014-04', 60486],
+                //             ['2014-05', 60500],
+                //             ['2014-06', 62908],
+                //             ['2014-07', 64818],
+                //             ['2014-08', 59961],
+                //             ['2014-09', 58542],
+                //             ['2014-10', 22050]
+                //         ],
+                //         incomplete_from: today.toISOString().substr(0, 7)
+                //     }
+                // ]
+            }
+
+            callback(null, graphData)
         },
     ], function(err, result) {
         if(err) {
