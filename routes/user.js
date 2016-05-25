7500446629ca4730b16317b5d7f48d21743e8d90
Argo Roots <argo@roots.ee>
2016-05-25 19:24:57 +0300

    /user/session

---
+++ routes/user.js
@@ -0,0 +1,24 @@
+var async  = require('async')
+var router = require('express').Router()
+
+var entu   = require('../helpers/entu')
+
+
+
+router.get('/session', function(req, res, next) {
+
+    if(!res.locals.user || !res.locals.user._id || !res.locals.user.key) { return next([403, 'No user!']) }
+
+    res.send({
+        result: {
+            key: res.locals.user._id + '.' + res.locals.user.key
+        },
+        version: APP_VERSION,
+        started: APP_STARTED
+    })
+
+})
+
+
+
+module.exports = router
