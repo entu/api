7d236af69badbe269580d9c36ee44c6dec33a330
Argo Roots <argo@roots.ee>
2016-12-13 13:22:40 +0200

    Create session from ID-Card auth

--- routes/auth/id-card.js
+++ routes/auth/id-card.js
@@ -1,6 +1,7 @@
-var _        = require('underscore')
-var op       = require('object-path')
-var router   = require('express').Router()
+var _      = require('underscore')
+var op     = require('object-path')
+var router = require('express').Router()
+var soap   = require('soap')
 
 var entu   = require('../../helpers/entu')
 
@@ -13,34 +14,46 @@ router.get('/', function(req, res) {
 
 
 router.get('/callback', function(req, res, next) {
-    res.send(req.headers)
-    // var user = {}
-    // var name = _.compact([
-    //     op.get(req, ['user', 'name', 'givenName']),
-    //     op.get(req, ['user', 'name', 'middleName']),
-    //     op.get(req, ['user', 'name', 'familyName'])
-    // ]).join(' ')
-    //
-    // op.set(user, 'provider', 'facebook')
-    // op.set(user, 'id', op.get(req, ['user', 'id']))
-    // op.set(user, 'name', name)
-    // op.set(user, 'email', op.get(req, ['user', 'emails', 0, 'value']))
-    // op.set(user, 'picture', op.get(req, ['user', 'photos', 0, 'value']))
-    //
-    // entu.addUserSession({
-    //     request: req,
-    //     user: user
-    // }, function(err, sessionId) {
-    //     if(err) { return next(err) }
-    //
-    //     var redirectUrl = req.cookies.redirect
-    //     if(redirectUrl) {
-    //         res.clearCookie('redirect')
-    //         res.redirect(redirectUrl + '?session=' + sessionId)
-    //     } else {
-    //         res.redirect('/auth/session/' + sessionId)
-    //     }
-    // })
+    if (req.headers.ssl_client_verify !== 'SUCCESS' || !req.headers.ssl_client_cert) {
+        return next(new Error('ID-Card error'))
+    }
+
+    var url = 'https://digidocservice.sk.ee/?wsdl'
+    var args = {
+        Certificate: req.headers.ssl_client_cert
+    }
+
+    soap.createClient(url, function(err, client) {
+        client.CheckCertificate(args, function(err, result) {
+            if(op.get(result, ['Status', '$value']) !== 'GOOD') { return next(new Error('Not valid ID-Card')) }
+
+            var user = {}
+            var name = _.compact([
+                op.get(result, ['UserGivenname', '$value']),
+                op.get(result, ['UserSurname', '$value'])
+            ]).join(' ')
+
+            op.set(user, 'provider', 'id-card')
+            op.set(user, 'id', op.get(result, ['UserIDCode', '$value']))
+            op.set(user, 'name', name)
+
+            entu.addUserSession({
+                request: req,
+                user: user
+            }, function(err, sessionId) {
+                if(err) { return next(err) }
+
+                var redirectUrl = req.cookies.redirect
+                if(redirectUrl) {
+                    res.clearCookie('redirect')
+                    res.redirect(redirectUrl + '?session=' + sessionId)
+                } else {
+                    res.redirect('/auth/session/' + sessionId)
+                }
+            })
+        })
+    })
+
 })
 
 
