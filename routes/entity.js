9317cf4098876becf4580886c3d9b90ac4525b27
Argo Roots <argo@roots.ee>
2017-08-10 15:10:33 +0300

    Show public entities

--- routes/entity.js
+++ routes/entity.js
@@ -15,7 +15,6 @@ router.get('/:entityId', function(req, res, next) {
     var entityId = entu.objectId(req.params.entityId)
 
     if (!entityId) { return next([422, new Error('Invalid Entity ID')]) }
-    if (!req.user) { return next([403, new Error('Forbidden')]) }
     if (!req.customer) { return next([400, new Error('No customer parameter')]) }
 
     async.waterfall([
@@ -23,12 +22,13 @@ router.get('/:entityId', function(req, res, next) {
             entu.dbConnection(req.customer, callback)
         },
         function(connection, callback) {
-            connection.collection('entity').findOne({ _id: entityId, _access: req.user }, { _mid: false, _access: false }, callback)
+            connection.collection('entity').findOne({ _id: entityId, _access: req.user }, { _mid: false }, callback)
         },
     ], function(err, entity) {
-        if(err) { return next(err) }
+        if (err) { return next(err) }
 
-        if(!entity) { return next([404, new Error('Entity not found')]) }
+        if (!entity) { return next([404, new Error('Entity not found')]) }
+        if (op.get(entity, '_access', []).indexOf(req.user) === -1 && op.get(entity, '_sharing.0.string', '') !== 'public') { return next([403, new Error('Forbidden')]) }
 
         res.respond(entity)
     })
