31279b2f42461705a5b4aaad0913bcd952dff2fc
Argo Roots <argo.roots@lhv.ee>
2018-03-08 10:51:27 +0200

    Get deleted and nondeleted in one db query

--- functions/account-get.js
+++ functions/account-get.js
@@ -20,64 +20,50 @@ exports.handler = (event, context, callback) => {
             },
 
             entities: (callback) => {
-                user.db.collection('entity').count(callback)
-            },
-            deletedEntities: (callback) => {
-                const aggregate = [
-                    { $match: { type: '_deleted', deleted: { $exists: false } } },
-                    { $group: { _id: '$entity', count: { $sum: 1 } } },
-                    { $group: { _id: null, count: { $sum: 1 } } }
-                ]
-                user.db.collection('property').aggregate(aggregate, (err, count) => {
-                    if (err) { return callback(err) }
-
-                    return callback(null, _.get(count, '0.count', 0))
-                })
+                user.db.collection('entity').aggregate([{
+                    $group: {
+                        _id: { $gt: ['$private._deleted', null] },
+                        count: { $sum: 1 }
+                    }
+                }], callback)
             },
 
             properties: (callback) => {
-                user.db.collection('property').find({ deleted: { $exists: false } }).count(callback)
-            },
-            deletedProperties: (callback) => {
-                user.db.collection('property').find({ deleted: { $exists: true } }).count(callback)
+                user.db.collection('property').aggregate([{
+                    $group: {
+                        _id: { $gt: ['$deleted', null] },
+                        count: { $sum: 1 }
+                    }
+                }], callback)
             },
 
             files: (callback) => {
-                const aggregate = [
-                    { $match: { size: { $exists: true }, deleted: { $exists: false } } },
-                    { $group: {_id: null, count: { $sum: 1 }, size: { $sum: '$size' } } },
-                    { $project: { _id: false } }
-                ]
-                user.db.collection('property').aggregate(aggregate, (err, count) => {
-                    if (err) { return callback(err) }
-
-                    return callback(null, _.get(count, '0', {}))
-                })
-            },
-            deletedFiles: (callback) => {
-                const aggregate = [
-                    { $match: { size: { $exists: true }, deleted: { $exists: true } } },
-                    { $group: {_id: null, count: { $sum: 1 }, size: { $sum: '$size' } } },
-                    { $project: { _id: false } }
-                ]
-                user.db.collection('property').aggregate(aggregate, (err, count) => {
-                    if (err) { return callback(err) }
-
-                    return callback(null, _.get(count, '0', {}))
-                })
+                user.db.collection('property').aggregate([
+                    {
+                        $match: {
+                            size: { $exists: true }
+                        }
+                    }, {
+                        $group: {
+                            _id: { $gt: ['$deleted', null] },
+                            count: { $sum: 1 },
+                            size: { $sum: '$size' }
+                        }
+                    }
+                ], callback)
             }
         }, (err, stats) => {
             if (err) { return callback(null, _h.error(err)) }
 
             callback(null, _h.json({
-                entities: stats.entities,
-                deletedEntities: stats.deletedEntities,
-                properties: stats.properties,
-                deletedProperties: stats.deletedProperties,
-                files: stats.files.count,
-                filesSize: stats.files.size,
-                deletedFiles: stats.deletedFiles.count,
-                deletedFilesSize: stats.deletedFiles.size,
+                entities: _.get(stats.entities.filter(e => e._id === false), '0.count', 0),
+                deletedEntities: _.get(stats.entities.filter(e => e._id === true), '0.count', 0),
+                properties: _.get(stats.properties.filter(e => e._id === false), '0.count', 0),
+                deletedProperties: _.get(stats.properties.filter(e => e._id === true), '0.count', 0),
+                files: _.get(stats.files.filter(e => e._id === false), '0.count', 0),
+                filesSize: _.get(stats.files.filter(e => e._id === false), '0.size', 0),
+                deletedFiles: _.get(stats.files.filter(e => e._id === true), '0.count', 0),
+                deletedFilesSize: _.get(stats.files.filter(e => e._id === true), '0.size', 0),
                 dbSize: stats.stats.dataSize + stats.stats.indexSize
             }))
         })
