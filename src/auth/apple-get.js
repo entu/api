f0d6c2bb233f18a8db82266e0043bef597a1d233
Argo Roots <argo@roots.ee>
2019-08-26 15:39:31 +0300

    Use POST handler for Apple auth

---
+++ src/auth/apple-get.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const _ = require('lodash')
+const _h = require('../_helpers')
+const jwt = require('jsonwebtoken')
+const querystring = require('querystring')
+
+exports.handler = async (event, context) => {
+  if (event.source === 'aws.events') { return }
+
+  try {
+  const params = _.get(event, 'queryStringParameters') || {}
+
+  const jwtSecret = await _h.ssmParameter('entu-api-jwt-secret')
+  const appleId = await _h.ssmParameter('entu-api-apple-id')
+
+    const state = jwt.sign({ next: params.next }, jwtSecret, {
+      audience: _.get(event, 'requestContext.identity.sourceIp'),
+      expiresIn: '5m'
+    })
+
+    const query = querystring.stringify({
+      client_id: appleId,
+      redirect_uri: `https://${event.headers.Host}${event.path}`,
+      response_type: 'code id_token',
+      response_mode: 'form_post',
+      scope: 'email name',
+      state: state
+    })
+
+    return _h.redirect(`https://appleid.apple.com/auth/authorize?${query}`, 302)
+  } catch (e) {
+    return _h.error(e)
+  }
+}
--- src/auth/apple-post.js
+++ src/auth/apple-post.js
@@ -10,7 +10,7 @@ exports.handler = async (event, context) => {
   if (event.source === 'aws.events') { return }
 
   try {
-    const params = _.get(event, 'queryStringParameters') || {}
+    const params = querystring.parse(event.body) || {}
 
     const jwtSecret = await _h.ssmParameter('entu-api-jwt-secret')
     const appleId = await _h.ssmParameter('entu-api-apple-id')
@@ -42,22 +42,6 @@ exports.handler = async (event, context) => {
       } else {
         return _h.json({ key: sessionId })
       }
-    } else {
-      const state = jwt.sign({ next: params.next }, jwtSecret, {
-        audience: _.get(event, 'requestContext.identity.sourceIp'),
-        expiresIn: '5m'
-      })
-
-      const query = querystring.stringify({
-        client_id: appleId,
-        redirect_uri: `https://${event.headers.Host}${event.path}`,
-        response_type: 'code id_token',
-        response_mode: 'form_post',
-        scope: 'email name',
-        state: state
-      })
-
-      return _h.redirect(`https://appleid.apple.com/auth/authorize?${query}`, 302)
     }
   } catch (e) {
     return _h.error(e)
