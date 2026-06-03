6d762772ea4bc4be65117e589cad8c025ccfd92a
Argo Roots <argo@roots.ee>
2026-06-03 14:57:55 +0300

    Enable Sentry performance sampling (10%)

--- .config/sentry.mjs
+++ .config/sentry.mjs
@@ -2,5 +2,6 @@ import * as Sentry from '@sentry/node'
 
 Sentry.init({
   dsn: process.env.NITRO_SENTRY_DSN,
-  release: process.env.NITRO_COMMIT_HASH
+  release: process.env.NITRO_COMMIT_HASH,
+  tracesSampleRate: 0.1
 })
