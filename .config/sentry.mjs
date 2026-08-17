831089508d6ac588e72987be3798fe841d1332f8
Argo Roots <argo@roots.ee>
2026-08-17 14:07:59 +0300

    Disable Sentry reporting in development

--- .config/sentry.mjs
+++ .config/sentry.mjs
@@ -2,6 +2,8 @@ import * as Sentry from '@sentry/node'
 
 Sentry.init({
   dsn: process.env.NITRO_SENTRY_DSN,
+  // Never report errors from local development
+  enabled: process.env.NODE_ENV !== 'development',
   release: process.env.NITRO_COMMIT_HASH,
   tracesSampleRate: 0.1
 })
